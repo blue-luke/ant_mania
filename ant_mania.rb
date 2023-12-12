c420860d632f3be48b9ecfa453a42fcb5f19d9fc
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2023-12-12 18:36:10 +0000

    feat: refactored, neatly moving in four directions

--- ant_mania.rb
+++ ant_mania.rb
@@ -1,9 +1,4 @@
 def ant_move(direction)
-  if direction == 'North'
-    return 'North'
-  elsif direction == 'South'
-    return 'South'
-  else
-    return 'East'
-  end
+  return direction
+
 end
--- spec/ant_mania_spec.rb
+++ spec/ant_mania_spec.rb
@@ -1,7 +1,7 @@
 require "./ant_mania.rb"
 
 
-describe " single ant" do 
+describe "single ant moves" do 
   it "moves north" do
     expect(ant_move('North')).to eq 'North'
   end
@@ -10,5 +10,8 @@ describe " single ant" do
   end
   it "moves south" do
     expect(ant_move'South').to eq 'South'
+  end
+  it "moves west" do
+    expect(ant_move'West').to eq 'West'
   end
 end
