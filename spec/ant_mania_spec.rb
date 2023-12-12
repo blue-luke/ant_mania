338354b3f1cdaca3c43c20a2f0e99aa3403015e2
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2023-12-12 18:40:20 +0000

    feat: correct case on directions: :

--- spec/ant_mania_spec.rb
+++ spec/ant_mania_spec.rb
@@ -3,15 +3,15 @@ require "./ant_mania.rb"
 
 describe "single ant moves" do 
   it "moves north" do
-    expect(ant_move('North')).to eq 'North'
+    expect(ant_move('north')).to eq 'north'
   end
   it "moves east" do
-    expect(ant_move'East').to eq 'East'
+    expect(ant_move'east').to eq 'east'
   end
   it "moves south" do
-    expect(ant_move'South').to eq 'South'
+    expect(ant_move'south').to eq 'south'
   end
   it "moves west" do
-    expect(ant_move'West').to eq 'West'
+    expect(ant_move'west').to eq 'west'
   end
 end
