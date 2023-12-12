05ec9834f8ee8ff638eada325eabdb5a5ddff108
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2023-12-12 20:17:58 +0000

    feat: basic ants fighting

--- ant_mania.rb
+++ ant_mania.rb
@@ -2,5 +2,8 @@ class Ant
   def move(position, direction)
     "ant moved to " + position[direction]
   end
+  def fight
+    "Omrida has been destroyed!"
+  end
 end 
 
--- spec/ant_mania_spec.rb
+++ spec/ant_mania_spec.rb
@@ -1,14 +1,27 @@
 require "./ant_mania.rb"
 
+kara = { "north"=> "Omrida", "south"=> "Celles", "east"=> "Chronoskis", "west"=> "Larvonthi" }
+omrida = { "north"=>"Cheabrenta", "south"=>"Kara", "east"=>"Turnep", "west"=>"Andomi" }
+cheabrenta = { "north" => "Uraro", "south" => "Omrida", "east" => "Trapist", "west" => "Heli" }
+
+
 describe "ant moves one colony" do
   it "moves north from Kara to Omrida" do
     ant = Ant.new
-    kara = { "north"=> "Omrida", "south"=> "Celles", "east"=> "Chronoskis", "west"=> "Larvonthi" }
     expect(ant.move(kara, 'north')).to eq 'ant moved to Omrida'
   end
   it "moves south from Omrida to Kara" do
     ant = Ant.new
-    omrida = { "north"=>"Cheabrenta", "south"=>"Kara", "east"=>"Turnep", "west"=>"Andomi" }
     expect(ant.move(omrida, 'south')).to eq 'ant moved to Kara'
+  end
+end
+
+describe "ants fight at same positions" do
+  it "fight each other" do
+    ant1 = Ant.new
+    ant2 = Ant.new
+    ant1.move(kara, 'north')
+    ant2.move(cheabrenta, 'south')
+    expect(ant2.fight()).to eq 'Omrida has been destroyed!'
   end
 end
