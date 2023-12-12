8304a44fb6a7e1a994e453d977315cbcd4010abc
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2023-12-12 19:34:54 +0000

    feat: refactor

--- ant_mania.rb
+++ ant_mania.rb
@@ -1,12 +1,4 @@
-def ant_move(direction)
-  kara = { "north"=> "Omrida", "south"=> "Celles", "east"=> "Chronoskis", "west"=> "Larvonthi" }
-  return "ant moved to " + kara[direction]
+def ant_move(position, direction)
+  "ant moved to " + position[direction]
 end
 
-def ant_move_again(direction)
-  if direction == "north"
-    return "ant moved to Cheabrenta"
-  else
-    return "ant moved to Kara"
-  end
-end
--- spec/ant_mania_spec.rb
+++ spec/ant_mania_spec.rb
@@ -1,26 +1,12 @@
 require "./ant_mania.rb"
 
 describe "ant moves one colony" do
-  it "moves north to Omrida" do
-    expect(ant_move('north')).to eq 'ant moved to Omrida'
+  it "moves north from Kara to Omrida" do
+    kara = { "north"=> "Omrida", "south"=> "Celles", "east"=> "Chronoskis", "west"=> "Larvonthi" }
+    expect(ant_move(kara, 'north')).to eq 'ant moved to Omrida'
   end
-  it "moves south to Celles" do
-    expect(ant_move('south')).to eq 'ant moved to Celles'
-  end
-  it "moves east to Chronoskis" do
-    expect(ant_move('east')).to eq 'ant moved to Chronoskis'
-  end
-end
-
-describe "ant moves two colonies" do
-  it "moves north to Omrida then north to Cheabrenta" do
-    ant_move('north')
-    expect(ant_move_again('north')).to eq 'ant moved to Cheabrenta'
-  end
-  it "moves north to Omrida then south to Kara" do
-    ant_move('north')
-    expect(ant_move_again('south')).to eq 'ant moved to Kara'
+  it "moves south from Omrida to Kara" do
+    omrida = { "north"=>"Cheabrenta", "south"=>"Kara", "east"=>"Turnep", "west"=>"Andomi" }
+    expect(ant_move(omrida, 'south')).to eq 'ant moved to Kara'
   end
 end
-
-# Omrida north=Cheabrenta south=Kara east=Turnep west=Andomi
